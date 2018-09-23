13d415e7d35a866bba8b3d095555dbe6a6af024f
asalinasj <andressalinasj@gmail.com>
2018-09-22 23:32:24 -0400

    Added file to temp
    Added as.js to temp

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+//import AS from '../../temp/as';
 
 class App extends Component {
   render() {
@@ -9,6 +10,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to SparkDev!</h1>
+          
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
