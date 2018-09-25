6e56f07b87f78ae192e7a2a2e14191a1226659e8
Mauricio <msant323@fiu.edu>
2018-09-25 14:14:32 -0400

    merge

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
-<<<<<<< HEAD
 // import HelloWorld from './temp/ac.js'
 
 import HelloC from './Component/HelloC'
 import Hello from './components/hello';
 import HelloWorld from './HelloWorld-temp';
 import HelloT from './components/helloT';
-=======
 import AndreaComponent from './AndreaComponent.js'
->>>>>>> andrea
 
 class App extends Component {
   render() {
@@ -23,13 +20,10 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-<<<<<<< HEAD
         <HelloC/>
         <HelloWorld />
         <HelloT/>
-=======
         <AndreaComponent/>
->>>>>>> andrea
       </div>
     );
   }
