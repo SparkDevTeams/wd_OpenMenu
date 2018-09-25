abc022260b67c022aa35faba854519c0c078cd17
Mia Gabb <miagabb@gmail.com>
2018-09-24 22:33:30 -0400

    make a component

--- temp/MG.js
+++ temp/MG.js
@@ -1,1 +1,14 @@
-Hello everyone
+import React, { Component } from 'react';
+import MG from '../temp/MG.js';
+
+class MG extends Component {
+    render(){
+        return(
+            <div>
+                <h1>Hello World from Mia!</h1>
+            </div>
+        );
+    }
+}
+
+export default MG;
