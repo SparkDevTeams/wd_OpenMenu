a06b5d4aaedf8c72ebf497bf5313c23cb68de79c
Andrea Teruel <andreateruel@Andreas-MacBook-Pro.local>
2018-09-25 13:46:15 -0400

    add andrea's new component

---
+++ frontend/src/AndreaComponent.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'; 
+
+class AndreaComponent extends Component{
+    render(){
+        return(
+            <div>
+            <h1>Hello from Andrea!</h1>
+            </div>
+        );
+      }
+    }
+
+export default AndreaComponent;
