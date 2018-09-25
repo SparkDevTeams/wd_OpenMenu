46a70665ed1a3e1a360c52887c2e3cc8b00c8f66
CristianCepeda <ccepe014@fiu.edu>
2018-09-25 15:28:57 -0400

    reset the project for people to redo Assignment 1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,32 +1,26 @@
-import React, { Component } from 'react';
-import './App.css';
-// import HelloWorld from './temp/ac.js'
+import React, { Component } from 'react'
+import logo from './assets/logo.svg'
+import './styles/App.css'
 
-import HelloC from './Component/HelloC'
-import Hello from './components/hello';
-import HelloWorld from './HelloWorld-temp';
-import HelloT from './components/helloT';
-import AndreaComponent from './AndreaComponent.js'
+/* Add your Components underneath */
+import CristianC from './components/CristianC'
 
 class App extends Component {
-  render() {
+  render () {
     return (
-      <div className="App">
-        <header className="App-header">
-          {/* <img src={logo} className="App-logo" alt="logo" /> */}
-          <h1 className="App-title">Welcome to SparkDev!</h1>
-          <Hello />
+      <div className='App'>
+        <header className='App-header'>
+          <img src={logo} className='App-logo' alt='logo' />
+          <h1 className='App-title'>Welcome to SparkDev!</h1>
         </header>
-        <p className="App-intro">
+        <p className='App-intro'>
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        <HelloC/>
-        <HelloWorld />
-        <HelloT/>
-        <AndreaComponent/>
+        {/* Add your Component underneath */}
+        <CristianC />
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
