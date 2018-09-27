8835deb9da53801c17f95e2e200190e36818a1c2
Mauricio <msant323@fiu.edu>
2018-09-26 23:15:13 -0400

    setup basic authentication, routing

---
+++ frontend/src/pages/Home/HomePV.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import Card from '@material-ui/core/Card'
+
+const LandingPV = (props) => {
+    return(
+        <div>
+        <Card>
+            <h1>Home Page!</h1>
+        </Card>
+      </div>
+    );
+}
+
+export default LandingPV;
