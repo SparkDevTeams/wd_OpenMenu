2d3a5830790df68705efbea5d8cde547cd0ecbe3
Soya Diaoune <soyadiaoune@Soyas-MacBook-Pro.local>
2018-09-27 15:12:01 -0400

    removed character

--- frontend/src/components/soya.js
+++ frontend/src/components/soya.js
@@ -8,7 +8,7 @@ class SoyasComponent extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Soya's Component!</h1>
+          <h1 className="App-title">Soyas Component!</h1>
         </header>
       </div>
     );
