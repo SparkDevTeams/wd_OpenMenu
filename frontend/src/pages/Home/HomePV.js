c3ab7cb19ef67cc2854003726981968f834182da
Mauricio <msant323@fiu.edu>
2018-09-29 18:31:03 -0400

    fix linting/prettier setup

--- frontend/src/pages/Home/HomePV.js
+++ frontend/src/pages/Home/HomePV.js
@@ -1,17 +1,17 @@
-import React from 'react'
+import React from 'react';
 
-import Card from '@material-ui/core/Card'
-import MauricioC from '../../components/MauricioC'
+import Card from '@material-ui/core/Card';
+import MauricioC from '../../components/MauricioC';
 
-const LandingPV = (props) => {
-    return(
-        <div>
-        <Card>
-            <h1>Home Page!</h1>
-            <MauricioC />
-        </Card>
-      </div>
-    );
-}
+const LandingPV = props => {
+  return (
+    <div>
+      <Card>
+        <h1>Home Page!</h1>
+        <MauricioC />
+      </Card>
+    </div>
+  );
+};
 
 export default LandingPV;
--- frontend/src/pages/Landing/LandingPC.js
+++ frontend/src/pages/Landing/LandingPC.js
@@ -1,60 +1,66 @@
-import React, { Component } from 'react'
-import LandingPV from './LandingPV'
-import AuthA from '../../store/actions/AuthA'
+import React, { Component } from 'react';
+import LandingPV from './LandingPV';
+import AuthA from '../../store/actions/AuthA';
 
-import {connect} from 'react-redux'
+import { connect } from 'react-redux';
 
 class LandingPC extends Component {
   state = {
     user: '',
-    password: '',
-  }
+    password: ''
+  };
 
-  onUserChange = (event) => {
-    this.setState({user: event.target.value})
-  }
+  onUserChange = event => {
+    this.setState({ user: event.target.value });
+  };
 
-  onPWChange = (event) => {
-    this.setState({password: event.target.value})
-  }
+  onPWChange = event => {
+    this.setState({ password: event.target.value });
+  };
 
   login = () => {
     let data = {
-      'email': this.state.user,
-      'password': this.state.password,
-    }
+      email: this.state.user,
+      password: this.state.password
+    };
     this.props.authFn.login(data);
-  }
+  };
 
   signup = () => {
     let data = {
-      'email': this.state.user,
-      'username': this.state.user,
-      'password': this.state.password,
-    }
+      email: this.state.user,
+      username: this.state.user,
+      password: this.state.password
+    };
     this.props.authFn.signup(data);
-  }
+  };
 
-  render () {
-    return (<LandingPV 
-              onUserChange={this.onUserChange}
-              onPWChange={this.onPWChange}
-              login={this.login}
-              signup={this.signup}
-              auth={this.props.auth} />)
+  render() {
+    return (
+      <LandingPV
+        onUserChange={this.onUserChange}
+        onPWChange={this.onPWChange}
+        login={this.login}
+        signup={this.signup}
+        auth={this.props.auth}
+      />
+    );
   }
 }
 
-const mapStateToProps = (state) => { 
+const mapStateToProps = state => {
   return {
-    auth: state.AuthR.auth,
-  }; 
+    auth: state.AuthR.auth
+  };
 };
 
 const mapDispatchToProps = dispatch => {
   return {
     authFn: AuthA(dispatch)
-  }
-}
+  };
+};
 
-export default connect(mapStateToProps, mapDispatchToProps)(LandingPC)
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(LandingPC);
