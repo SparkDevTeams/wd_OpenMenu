56fbb2148f20ceacf13a46282d30a7031a4df3f8
asalinasj <andressalinasj@gmail.com>
2018-09-30 17:17:07 -0400

    Create new component on HomePV

--- frontend/src/pages/Home/HomePV.js
+++ frontend/src/pages/Home/HomePV.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import Card from '@material-ui/core/Card';
 import MauricioC from '../../components/MauricioC';
+import AndresC from '../../components/AndresC';
 
 const LandingPV = props => {
   return (
@@ -9,6 +10,7 @@ const LandingPV = props => {
       <Card>
         <h1>Home Page!</h1>
         <MauricioC />
+        <AndresC />
       </Card>
     </div>
   );
