8a9646825d275ef020918f483ca4da983e2fe675
Andrea Teruel <andreateruel@Andreas-MacBook-Pro.local>
2018-10-01 18:56:32 -0400

    create new component

--- frontend/src/pages/Home/HomePV.js
+++ frontend/src/pages/Home/HomePV.js
@@ -1,12 +1,14 @@
 import React from 'react'
 
 import Card from '@material-ui/core/Card'
+import AndreaC from '../../components/AndreaC' 
 
 const LandingPV = (props) => {
     return(
         <div>
         <Card>
             <h1>Home Page!</h1>
+            <AndreaC/>
         </Card>
       </div>
     );
