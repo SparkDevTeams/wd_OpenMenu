927681c10965e59bccb5c98794b5547623fb8e56
Amy <amy.sudarat@gmail.com>
2018-10-02 00:20:47 -0400

    finish assignment#2

--- frontend/src/pages/Home/HomePV.js
+++ frontend/src/pages/Home/HomePV.js
@@ -1,14 +1,14 @@
 import React from 'react';
 
 import Card from '@material-ui/core/Card';
-import MauricioC from '../../components/MauricioC';
+import AmyC from '../../components/AmyC';
 
 const LandingPV = props => {
   return (
     <div>
       <Card>
         <h1>Home Page!</h1>
-        <MauricioC />
+        <AmyC />
       </Card>
     </div>
   );
