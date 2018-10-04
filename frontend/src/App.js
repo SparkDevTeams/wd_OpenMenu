6a5602e4a9da3b6e1fb3f212e534425be6121d78
Soya Diaoune <soyadiaoune@Soyas-MacBook-Pro.local>
2018-10-04 15:59:17 -0400

    routing and navigation menu

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,12 @@ import NavbarC from "./components/Navbar/NavbarC";
 import LandingPC from "./pages/Landing/LandingPC";
 import HomePC from "./pages/Home/HomePC";
 
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+// import { Toolbar } from "@material-ui/core";
+
 class App extends Component {
   render() {
     let margin = {
@@ -12,6 +18,7 @@ class App extends Component {
     };
 
     return (
+      <div>
       <BrowserRouter>
         <div className="App">
           <NavbarC />
@@ -20,10 +27,12 @@ class App extends Component {
               <Route exact path="/" component={LandingPC} />
               <Route path="/welcome" component={LandingPC} />
               <Route path="/home" component={HomePC} />
+              {/* <Route path="" component={} /> */}
             </Switch>
           </div>
         </div>
       </BrowserRouter>
+      </div>
     );
   }
 }
