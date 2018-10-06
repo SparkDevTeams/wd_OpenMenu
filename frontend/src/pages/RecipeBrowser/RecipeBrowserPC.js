c4e3a037999c063afc1a8a28b8f16040225ee7ec
Andrea Teruel <andreateruel@Andreas-MacBook-Pro.local>
2018-10-06 14:52:56 -0400

    add new page

---
+++ frontend/src/pages/RecipeBrowser/RecipeBrowserPC.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import RecipeBrowserPV from "./RecipeBrowserPV";
+
+class RecipeBrowserPC extends Component {
+  render() {
+    return <RecipeBrowserPV />;
+  }
+}
+
+export default RecipeBrowserPC;
---
+++ frontend/src/pages/RecipeBrowser/RecipeBrowserPV.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const RecipeBrowserPV = props => {
+  return (
+    <div>
+      <h1> This is the recipe page!</h1>
+    </div>
+  );
+};
+
+export default RecipeBrowserPV;
