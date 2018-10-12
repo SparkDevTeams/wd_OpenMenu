d3bf57c8b4b6a3ff97a5bde9e43a7f14c89136f1
Mauricio <msant323@fiu.edu>
2018-10-12 18:02:38 -0400

    remove imported exercise components from HomePV

--- frontend/src/pages/Home/HomePV.js
+++ frontend/src/pages/Home/HomePV.js
@@ -1,33 +1,9 @@
 import React from "react";
 
-import JabinC from "../../components/JabinC"
-import AndresC from "../../components/AndresC";
-import ElliotC from "../../components/ElliotC";
-import Card from "@material-ui/core/Card";
-import MauricioC from "../../components/MauricioC";
-import CristianC from "../../components/CristianC";
-import OctavioC from "../../components/OctavioC";
-import MiaC from "../../components/MiaC";
-import TommyC from "./../../components/TommyC";
-import AmyC from "../../components/AmyC";
-import SoyaD from "../../components/SoyaD";
-
 const LandingPV = props => {
   return (
     <div>
-      <Card>
-        <h1>Home Page!</h1>
-        <JabinC />
-        <CristianC />
-        <AmyC />
-        <MiaC />
-        <MauricioC />
-        <AndresC />
-        <ElliotC />
-        <TommyC />
-        <OctavioC />
-        <SoyaD />
-      </Card>
+      <h1>Home Page!</h1>
     </div>
   );
 };
