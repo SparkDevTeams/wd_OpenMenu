b4ab20c91579e9781c4f6e92b35cc7fdf3624f4b
Mauricio <msant323@fiu.edu>
2018-10-12 19:44:42 -0400

    homePC subscribe to state and getRecipes from backend via action

--- frontend/src/pages/Home/HomePV.js
+++ frontend/src/pages/Home/HomePV.js
@@ -1,11 +1,16 @@
 import React from "react";
+import RecipeCard from "./../../components/Recipe/ItemCardV";
 
-const LandingPV = props => {
+const HomePV = props => {
   return (
     <div>
       <h1>Home Page!</h1>
+      {props.recipes.map(recipe => (
+        <h1>{recipe.name}</h1>
+      ))}
+      <RecipeCard name={props.recipes.name} />
     </div>
   );
 };
 
-export default LandingPV;
+export default HomePV;
