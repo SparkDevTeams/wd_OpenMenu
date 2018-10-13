d806a34c41417b172bf9d28dce431b3392b88fc0
Tommy <carrascalx@gmail.com>
2018-10-13 16:30:04 -0400

    added algorithm

--- backend/algorithm.js
+++ backend/algorithm.js
@@ -1,18 +1,20 @@
-'use strict';
-let menu = {
+/* eslint-disable */
+const axios = require('axios');
+
+let Recipe = {
   name: "Tommy's Breakfast!",
   user: 'Tommy C',
 };
 
-let name = menu.name;
-let user = menu.user;
+let name = Recipe.name;
+let user = Recipe.user;
 
 let newName = name.replace(/\W/g, '');
 let newUser = user.replace(/\W/g, '');
 
 let date = Date.now();
 
-let id = date + newName + newUser;
+id = date + newName + newUser;
 
 console.log(id);
 
