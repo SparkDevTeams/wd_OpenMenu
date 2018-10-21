9450c2cb0da4eefde2f289912b78be209f6d68df
Amy <amy.sudarat@gmail.com>
2018-10-21 16:11:28 -0400

    Recipe card show image from S3

--- frontend/src/components/Recipe/RecipeCardV.js
+++ frontend/src/components/Recipe/RecipeCardV.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { Component } from "react";
+import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 import { withStyles } from "@material-ui/core/styles";
@@ -21,39 +22,66 @@ const styles = {
   }
 };
 
-function MediaCard(props) {
-  const { classes } = props;
-  return (
-    <Card className={classes.card}>
-      <CardActionArea>
-        <CardMedia
-          className={classes.media}
-          image={props.recipe.image}
-          title={props.recipe.name}
-        />
-        <CardContent>
-          <Typography gutterBottom variant="headline" component="h2">
-            {props.recipe.name}
-          </Typography>
-          <Typography component="p">{props.recipe.description}</Typography>
-        </CardContent>
-      </CardActionArea>
-      {/* <CardActions>
-        <Button size="small" color="primary">
-          Share
-        </Button>
-        <Link to="/recipes/recipe">
-          <Button size="small" color="primary">
-            Learn More
-          </Button>
-        </Link>
-      </CardActions> */}
-    </Card>
-  );
+class MediaCard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      // uid
+      image: "",
+      // image data
+      image_data: null,
+      userRecipeImages: []
+    };
+  }
+
+  componentWillMount() {
+    this.setState({ image: this.props.recipe.image });
+    this.setState({ userRecipeImages: this.props.userRecipeImages });
+  }
+
+  getImage = img_name => {
+    for (let i = 0; i < this.props.userRecipeImages.length; i++) {
+      if (this.props.userRecipeImages[i].name === img_name) {
+        this.setState({ image_data: this.props.userRecipeImages[i].data });
+        break;
+      } // end if
+    } // end for
+  }; // end getImage()
+
+  render() {
+    if (this.props.userRecipeImages.length && this.state.image_data === null) {
+      this.getImage(this.state.image);
+    }
+    return (
+      <Card className={this.props.classes.card}>
+        <CardActionArea>
+          <CardMedia
+            className={this.props.classes.media}
+            image={this.state.image_data}
+            title={this.props.recipe.name}
+          />
+          <CardContent>
+            <Typography gutterBottom variant="headline" component="h2">
+              {this.props.recipe.name}
+            </Typography>
+            <Typography component="p">
+              {this.props.recipe.description}
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+      </Card>
+    );
+  }
 }
 
 MediaCard.propTypes = {
   classes: PropTypes.object.isRequired
 };
 
-export default withStyles(styles)(MediaCard);
+const mapStateToProps = state => {
+  return {
+    userRecipeImages: state.RecipeR.userRecipeImages
+  };
+};
+
+export default withStyles(styles)(connect(mapStateToProps)(MediaCard));
