9b3969d3e90fec6f889c674e0149311d5ef10134
Tommy <carrascalx@gmail.com>
2018-11-02 21:42:02 -0400

    made home page more responsive

--- frontend/src/pages/Home/HomePV.js
+++ frontend/src/pages/Home/HomePV.js
@@ -7,64 +7,50 @@ import MenuCard from "../../components/Menu/MenuCard";
 import RecipeCardV from "../../components/Recipe/RecipeCardV";
 import ItemCard from "./../../components/Item/ItemC";
 import "./style.css";
-const styles = {
-  root: {
-    flexGrow: 1
-  }
-};
 
 const HomePV = props => {
   const { classes } = props;
-  let containerSpacing = 12;
-  let itemSpacing = 12;
-  let cardSpacing = 16;
-  let menu_cardRowWidth = 4;
-  let recipe_cardRowWidth = 5;
-  let items_cardRowWidth = 6;
+
+  const styles = {
+    "margin-left": 15
+  };
 
   return (
-    // This part need better styling
-    <Grid container spacing={containerSpacing}>
-      <Grid item xs={itemSpacing}>
-        <h1>Menus</h1>
-        <Grid container spacing={cardSpacing}>
-          {props.userMenus.map(menu => (
-            <Grid key={menu.uid} item xs={menu_cardRowWidth}>
-              <Link
-                className={classes.cardStyle}
-                to={{ pathname: "/menus/" + menu.uid }}
-              >
-                <MenuCard menu={menu} />
-              </Link>
-            </Grid>
-          ))}
-        </Grid>
+    <div>
+      <h1>Menus</h1>
+      <div className="container">
+        {props.userMenus.map(menu => (
+          <Link to={{ pathname: "/menus/" + menu.uid }}>
+            <div className="card">
+              <MenuCard menu={menu} />
+            </div>
+          </Link>
+        ))}
+      </div>
 
-        <h1>Recipes</h1>
-        <Grid container spacing={cardSpacing}>
-          {props.userRecipes.map(recipe => (
-            <Grid key={recipe.uid} item xs={recipe_cardRowWidth}>
-              <Link
-                className={classes.cardStyle}
-                to={{ pathname: "/recipes/" + recipe.uid }}
-              >
-                <RecipeCardV recipe={recipe} large={false} />
-              </Link>
-            </Grid>
-          ))}
-        </Grid>
-
-        <h1>Items</h1>
-        <Grid container spacing={cardSpacing}>
-          {props.userItems.map(item => (
-            <Grid key={item.uid} item xs={items_cardRowWidth}>
-              <ItemCard itemId={item.uid} amount={0.0} />
-            </Grid>
-          ))}
-        </Grid>
-      </Grid>
-    </Grid>
+      <h1>Recipes</h1>
+      <div className="container">
+        {props.userRecipes.map(recipe => (
+          <Link
+            // className={classes.cardStyle}
+            to={{ pathname: "/recipes/" + recipe.uid }}
+          >
+            <div className="card recipe">
+              <RecipeCardV recipe={recipe} large={false} />
+            </div>
+          </Link>
+        ))}
+      </div>
+      <h1>Items</h1>
+      <div style={styles} className="container">
+        {props.userItems.map(item => (
+          <div className="card">
+            <ItemCard itemId={item.uid} amount={0.0} />
+          </div>
+        ))}
+      </div>
+    </div>
   );
 };
 
-export default withStyles(styles)(HomePV);
+export default HomePV;
