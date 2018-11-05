26b4fd520b89994c60994acaed3e4762ec1b8d61
mauricio s <msant323@fiu.edu>
2018-11-05 14:16:37 -0500

    minor fix to copy

--- frontend/src/pages/Landing/LandingPV.js
+++ frontend/src/pages/Landing/LandingPV.js
@@ -15,38 +15,79 @@ const LandingPV = props => {
   } else {
     return (
       <div className="container-landing">
-    
-
         <Card id="welcome-card">
-        <img
-          src={Logo}
-          alt="Whooops!"
-          style={{ width: "50%", height: "50%", flex: 1, minHeight: 80, minWIdth: 80, maxHeight: 200 }}
-        />
-        <h2 style={{marginTop: 10, marginBottom: 3}}>Open Menu</h2>
+          <img
+            src={Logo}
+            alt="Whooops!"
+            style={{
+              width: "50%",
+              height: "50%",
+              flex: 1,
+              minHeight: 80,
+              minWIdth: 80,
+              maxHeight: 200
+            }}
+          />
+          <h2 style={{ marginTop: 10, marginBottom: 3 }}>Open Menu</h2>
 
-        <div  style={{display: !props.showLogin ? 'initial' : 'none', paddingBottom: '20px'}}>
-          <div id="checkmark-container">
-          <p><svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18"><path d="M6.61 11.89L3.5 8.78 2.44 9.84 6.61 14l8.95-8.95L14.5 4z"/></svg>
-          Create and share Menu's & Recipes</p>
-          <p><svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18"><path d="M6.61 11.89L3.5 8.78 2.44 9.84 6.61 14l8.95-8.95L14.5 4z"/></svg>
-          Manage your shopping list</p>
-          <p>
-          <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18"><path d="M6.61 11.89L3.5 8.78 2.44 9.84 6.61 14l8.95-8.95L14.5 4z"/></svg>find the
-            best deals near you...</p>
-          </div>
-      
-          <Button
-            variant="contained"
-            color="primary"
-            onClick={props.toggleLogin}
-            style={{paddingTop: 5}}
+          <div
+            style={{
+              display: !props.showLogin ? "initial" : "none",
+              paddingBottom: "20px"
+            }}
+          >
+            <div id="checkmark-container">
+              <p>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="18"
+                  height="18"
+                  viewBox="0 0 18 18"
+                >
+                  <path d="M6.61 11.89L3.5 8.78 2.44 9.84 6.61 14l8.95-8.95L14.5 4z" />
+                </svg>
+                Create and share Menu's & Recipes
+              </p>
+              <p>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="18"
+                  height="18"
+                  viewBox="0 0 18 18"
+                >
+                  <path d="M6.61 11.89L3.5 8.78 2.44 9.84 6.61 14l8.95-8.95L14.5 4z" />
+                </svg>
+                Manage your shopping list
+              </p>
+              <p>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="18"
+                  height="18"
+                  viewBox="0 0 18 18"
+                >
+                  <path d="M6.61 11.89L3.5 8.78 2.44 9.84 6.61 14l8.95-8.95L14.5 4z" />
+                </svg>
+                Find the best deals near you
+              </p>
+            </div>
+
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={props.toggleLogin}
+              style={{ paddingTop: 5 }}
             >
-            Get Started
-          </Button>
-        </div>
-        <div style={{display: props.showLogin ? 'initial' : 'none', paddingBottom: '20px'}}>
-          <CardActionArea >
+              Get Started
+            </Button>
+          </div>
+          <div
+            style={{
+              display: props.showLogin ? "initial" : "none",
+              paddingBottom: "20px"
+            }}
+          >
+            <CardActionArea>
               <CardContent>
                 <form>
                   <TextField
